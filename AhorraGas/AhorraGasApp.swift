c02140c9d9ff81663e22945634784ed93a2b699b
Anticlub <cristofdzfdz@gmail.com>
2026-05-24 17:09:39 +0200

    Initial Commit

---
+++ AhorraGas/AhorraGasApp.swift
@@ -0,0 +1,17 @@
+//
+//  AhorraGasApp.swift
+//  AhorraGas
+//
+//  Created by cristofer fernandez on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct AhorraGasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AhorraGasTests/AhorraGasTests.swift
@@ -0,0 +1,19 @@
+//
+//  AhorraGasTests.swift
+//  AhorraGasTests
+//
+//  Created by cristofer fernandez on 24/05/2026.
+//
+
+import Testing
+@testable import AhorraGas
+
+struct AhorraGasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
