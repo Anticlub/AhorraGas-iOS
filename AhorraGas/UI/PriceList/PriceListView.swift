42b075618f6fc79d80d59116dabe101648b5f442
Anticlub <cristofdzfdz@gmail.com>
2026-09-01 20:31:14 +0200

    feat: add Station.samples sample data
    Estaciones de ejemplo reutilizables para previews y desarrollo (temporal,
    hasta que exista la capa de datos real).

---
+++ AhorraGas/UI/PriceList/PriceListView.swift
@@ -0,0 +1,18 @@
+//
+//  PriceListView.swift
+//  AhorraGas
+//
+//  Created by cristofer fernandez on 01/09/2026.
+//
+
+import SwiftUI
+
+struct PriceListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PriceListView()
+}
